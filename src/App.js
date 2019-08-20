bc78fac85376493f17b4c5def9f2f89da5b5dccb
Daniel Wang <daniel@loopring.org>
2019-08-20 07:57:09 -0400

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+
+import {Button} from 'react-bulma-components/full';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Button
+      renderAs="a"
+      href="https://alligator.io"
+      color="danger"
+      size="large"
+      rounded
+      outlined
+    ></Button>
   );
 }
 
