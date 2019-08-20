d6a56171317759256d7e7b183ec640c36c90f6a0
Daniel Wang <daniel@loopring.org>
2019-08-20 12:25:13 -0400

    hacking layout

--- src/Protocol/SectionHackStory.js
+++ src/Protocol/SectionHackStory.js
@@ -4,7 +4,23 @@ import './SectionHackStory.scss';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faUserSecret} from '@fortawesome/free-solid-svg-icons';
 
+const API = 'https://hn.algolia.com/api/v1/s';
+
 class SectionHackStory extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      stories: [{title: 'afdf', detail: 'fdafaf', date: 'aaaa'}],
+    };
+  }
+
+  componentDidMount() {
+    fetch(API)
+        .then((response) => response.json())
+        .then((data) => this.setState({stories: data.stories}));
+  }
+
   render() {
     return (
       <section className="section section-hack-stories is-large has-text-white">
@@ -19,97 +35,15 @@ class SectionHackStory extends React.Component {
             </div>
           </div>
           <div className="columns is-multiline">
-            <div className="column is-2">
-              <div className="content">
-                <h2>Zaif, $60,000,000</h2>
-                <p>
-                  Security breachon Zaif'sexhcange,$60m stolen from user's hot
-                  wallet.
-                </p>
+            {this.state.stories.map((story) => (
+              <div className="column is-2">
+                <div className="content">
+                  <h2>{story.title}</h2>
+                  <i>{story.date}</i>
+                  <p>{story.detail}}</p>
+                </div>
               </div>
-            </div>
-
-            <div className="column is-2">
-              <div className="content">
-                <h2>Zaif, $60,000,000</h2>
-                <p>
-                  Security breachon Zaif'sexhcange, $60m stolen from user's hot
-                  wallet.
-                </p>
-              </div>
-            </div>
-
-            <div className="column is-2">
-              <div className="content">
-                <h2>Zaif, $60,000,000</h2>
-                <i>2019/29/12</i>
-                <p>
-                  Security breachon Zaif'sexhcange, $60m stolen from user's hot
-                  wallet.
-                </p>
-              </div>
-            </div>
-
-            <div className="column is-2">
-              <div className="content">
-                <h2>Zaif, $60,000,000</h2>
-                <p>
-                  Security breachon Zaif'sexhcange,$60m stolen from user's hot
-                  wallet.
-                </p>
-              </div>
-            </div>
-
-            <div className="column is-2">
-              <div className="content">
-                <h2>Zaif, $60,000,000</h2>
-                <p>
-                  Security breachon Zaif'sexhcange,$60m stolen from user's hot
-                  wallet.
-                </p>
-              </div>
-            </div>
-
-            <div className="column is-2">
-              <div className="content">
-                <h2 className="time">Zaif, $60,000,000</h2>
-                <h2>Zaif, $60,000,000</h2>
-                <p>
-                  Security breachon Zaif'sexhcange,$60m stolen from user's hot
-                  wallet.
-                </p>
-              </div>
-            </div>
-
-            <div className="column is-2">
-              <div className="content">
-                <h2>Zaif, $60,000,000</h2>
-                <p>
-                  Security breachon Zaif'sexhcange,$60m stolen from user's hot
-                  wallet.
-                </p>
-              </div>
-            </div>
-
-            <div className="column is-2">
-              <div className="content">
-                <h2>Zaif, $60,000,000</h2>
-                <p>
-                  Security breachon Zaif'sexhcange,$60m stolen from user's hot
-                  wallet.
-                </p>
-              </div>
-            </div>
-
-            <div className="column is-2">
-              <div className="content">
-                <h2>Zaif, $60,000,000</h2>
-                <p>
-                  Security breachon Zaif'sexhcange,$60m stolen from user's hot
-                  wallet.
-                </p>
-              </div>
-            </div>
+            ))}
           </div>
         </div>
       </section>
