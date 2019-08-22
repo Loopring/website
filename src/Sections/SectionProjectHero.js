1e236e974ab9dc267cc05924c353f0e260290dad
Daniel Wang <daniel@loopring.org>
2019-08-22 18:29:41 -0400

    hero

--- src/Sections/SectionProjectHero.js
+++ src/Sections/SectionProjectHero.js
@@ -13,8 +13,25 @@ class SectionProjectHero extends React.Component {
 
   render() {
     return (
-      <section className="section  section-project-hero is-large">
-        <div class="backdrop"></div>
+      <section className="section section-project-hero is-large">
+        <div className="backdrop"></div>
+
+        <div className="text">
+          <svg>
+            <defs>
+              <mask id="mask" x="0" y="0" width="100%" height="100%">
+                <rect id="alpha" x="0" y="0" width="100%" height="100%" />
+                <text id="title" x="50%" y="30%" dy="1.58em">
+                  LOOPRING
+                </text>
+                <text id="subtitle" x="50%" y="35%" dy="9.8em">
+                  A High-Performance Ethereum DEX Protocol
+                </text>
+              </mask>
+            </defs>
+            <rect id="base" x="0" y="0" width="100%" height="100%" />
+          </svg>
+        </div>
       </section>
     );
   }
