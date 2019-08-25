4336c627e7f9ba7230d216ca7a95142fb23ec99c
Daniel Wang <daniel@loopring.org>
2019-08-24 23:02:46 -0400

    add content to lrc page

--- src/LRC.js
+++ src/LRC.js
@@ -53,44 +53,28 @@ class LRC extends React.Component {
             </div>
           </div>
         </section>
-
         <section className="section section-lrc-utilities is-large has-text-white has-background-black">
           <div className="container is-centered has-text-centered">
             <div className="columns is-centered">
-              <div data-aos="fade-up" className="column is-10 has-text-left">
+              <div
+                data-aos="fade-up"
+                className="column is-10 has-text-centered"
+              >
                 <h1>Utilities</h1>
               </div>
             </div>
+
             <div className="columns is-centered is-multiline">
-              <div data-aos="fade-up" className="column  is-5 has-text-left">
+              <div data-aos="fade-up" className="column  is-4 has-text-right">
                 <div className="content">
-                  <h2> Loopring 1.0</h2>
-                  <p>
-                    Loopring 1.0 introduced the <i>Ring-Matching</i> concept —
-                    in one trade, up to 12 orders can circularly swap
-                    crypto-assets in the form of <i>ring</i>s. This feature is
-                    unique and brings the potential for improved price discovery
-                    and higher trading volume.
-                  </p>
-                  <p>
-                    Our project name "Loopring" actually comes after this
-                    feature.
-                  </p>
-                  <p>
-                    With 1.0, orders are managed and matched off-chain. The
-                    protocol's smart contracts verify order signatures, trading
-                    prices, and settlement amounts on-chain and perform token
-                    transfers for each order accordingly. Because of the
-                    off-chain order management and matching, the performance of
-                    Loopring 1.0 is better than that of EtherDelta and the like.
-                  </p>
+                  <div className="category is-unselectable">#1</div>
                 </div>
               </div>
 
-              <div data-aos="fade-up" className="column  is-5 has-text-left">
+              <div data-aos="fade-up" className="column  is-4 has-text-left">
                 <div className="content">
-                  <h2> Loopring 1.5</h2>
-
+                  {' '}
+                  <h2>fdfda fldja fdaf </h2>
                   <p>
                     Loopring 1.5 introduced another unique feature called{' '}
                     <i>Dual Authoring</i> to prevent front-running. Each order
@@ -100,10 +84,34 @@ class LRC extends React.Component {
                     use it to sign rings that include the order.
                   </p>
                   <p>
-                    Dual Authoring effectively prevents pending settlements and
-                    their orders from being stolen by middlemen in the Ethereum
-                    network, as those middlemen have no access to the orders' DA
-                    private keys.
+                    Dual Authoring comes very handy for scenarios where orders
+                    can be matched by more than one exchange/relayer.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className="section section-lrc-utilities is-large has-text-white has-background-black">
+          <div className="container is-centered has-text-centered">
+            <div className="columns is-centered">
+              <div data-aos="fade-up" className="column  is-4 has-text-right">
+                <div className="content">
+                  <div className="category is-unselectable">#2</div>
+                </div>
+              </div>
+
+              <div data-aos="fade-up" className="column  is-4 has-text-left">
+                <div className="content">
+                  {' '}
+                  <h2>fdfda fldja fdaf </h2>
+                  <p>
+                    Loopring 1.5 introduced another unique feature called{' '}
+                    <i>Dual Authoring</i> to prevent front-running. Each order
+                    has a randomly generated DA key-pair — the public key is
+                    part of the order; the private key is not part of the order
+                    but is sent to the relayer so that the relayer can and must
+                    use it to sign rings that include the order.
                   </p>
                   <p>
                     Dual Authoring comes very handy for scenarios where orders
@@ -111,27 +119,63 @@ class LRC extends React.Component {
                   </p>
                 </div>
               </div>
-
-              <div data-aos="fade-up" className="column  is-5  has-text-left">
+            </div>
+          </div>
+        </section>
+        <section className="section section-lrc-utilities is-large has-text-white has-background-black">
+          <div className="container is-centered has-text-centered">
+            <div className="columns is-centered">
+              <div data-aos="fade-up" className="column  is-4 has-text-right">
                 <div className="content">
-                  <h2> Loopring 2.0</h2>
+                  <div className="category is-unselectable">#3</div>
+                </div>
+              </div>
 
+              <div data-aos="fade-up" className="column  is-4 has-text-left">
+                <div className="content">
+                  {' '}
+                  <h2>fdfda fldja fdaf </h2>
                   <p>
-                    With the 2.0 release, Loopring no longer requires traders to
-                    pay relayers LRC, Loopring's utility token, as the trading
-                    fee. The trading fees can be paid using any ERC-20 tokens
-                    that relayers specify. Relayers only have to use a
-                    percentage, known as the burn-rate, of their fee income to
-                    buy LRC to burn (so LRC's total supply is ever decreasing).
-                    Different tokens have varying burn-rates, and the burn rates
-                    for LRC and WETH are the lowest, which makes paying in LRC
-                    and Ether the most cost-efficient.
+                    Loopring 1.5 introduced another unique feature called{' '}
+                    <i>Dual Authoring</i> to prevent front-running. Each order
+                    has a randomly generated DA key-pair — the public key is
+                    part of the order; the private key is not part of the order
+                    but is sent to the relayer so that the relayer can and must
+                    use it to sign rings that include the order.
                   </p>
                   <p>
-                    This <i>Any-Token-As-Fee</i> feature not only significantly
-                    reduces frictions in user experience, but also enables
-                    decentralized exchanges to use own their platform token as
-                    the fee token.
+                    Dual Authoring comes very handy for scenarios where orders
+                    can be matched by more than one exchange/relayer.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className="section section-lrc-utilities is-large has-text-white has-background-black">
+          <div className="container is-centered has-text-centered">
+            <div className="columns is-centered">
+              <div data-aos="fade-up" className="column  is-4 has-text-right">
+                <div className="content">
+                  <div className="category is-unselectable">#4</div>
+                </div>
+              </div>
+
+              <div data-aos="fade-up" className="column  is-4 has-text-left">
+                <div className="content">
+                  {' '}
+                  <h2>fdfda fldja fdaf </h2>
+                  <p>
+                    Loopring 1.5 introduced another unique feature called{' '}
+                    <i>Dual Authoring</i> to prevent front-running. Each order
+                    has a randomly generated DA key-pair — the public key is
+                    part of the order; the private key is not part of the order
+                    but is sent to the relayer so that the relayer can and must
+                    use it to sign rings that include the order.
+                  </p>
+                  <p>
+                    Dual Authoring comes very handy for scenarios where orders
+                    can be matched by more than one exchange/relayer.
                   </p>
                 </div>
               </div>
