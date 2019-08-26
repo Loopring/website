384462cb7ac429fc3a31ce010ae7c4e60b658f0d
Daniel Wang <daniel@loopring.org>
2019-08-26 17:57:59 -0400

    add content to version 3 page

--- src/Sections/SectionLoopringV30.js
+++ src/Sections/SectionLoopringV30.js
@@ -19,22 +19,13 @@ class SectionLoopringV30 extends React.Component {
         <div className="container">
           <div className="columns is-centered is-multiline">
             <div data-aos="fade-up" className="column is-12  has-text-centered">
-              <h1> Loopring 3.0</h1>
+              <h1>Design and Building Blocks</h1>
             </div>
             <div data-aos="fade-up" className="column is-5">
-              <h2>Fully Open-Sourced</h2>
+              <h2>Design Doc</h2>
               <div className="content">
-                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
-                commodo ligula eget dolor. Aenean massa. Cum sociis natoque
-                penatibus et magnis dis parturient montes, nascetur ridiculus
-                mus. Donec quam felis, ultricies nec, pellentesque eu, pretium
-                quis, sem. Nulla consequat massa quis enim. Donec pede justo,
-                fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo,
-                rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum
-                felis eu pede mollis pretium. Integer tincidunt. Cras dapibus.
-                Vivamus elementum semper nisi. Aenean vulputate eleifend tellus.
-                Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac,
-                enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a,
+                There is no better source than our Design Doc for you to learn
+                about all the ideas behind Loopring 3.0.
               </div>
               <div className="content">
                 Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
@@ -49,13 +40,107 @@ class SectionLoopringV30 extends React.Component {
                 Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac,
                 enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a,
               </div>
+            </div>
 
-              <div className="content">Check out our code on GitHub:</div>
+            <div data-aos="fade-up" className="column is-5">
+              <h2>Resources</h2>
               <ul className="links">
                 <li>
                   {' '}
                   ➜{' '}
-                  <a className="highlight" href="">
+                  <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="highlight"
+                    href="https://ethresear.ch/t/on-chain-scaling-to-potentially-500-tx-sec-through-mass-tx-validation/3477"
+                  >
+                    Vitalik's post on zkRollup
+                    ···············································
+                    <span className="symboled"> ◎</span>
+                  </a>
+                </li>
+
+                <li>
+                  {' '}
+                  ➜{' '}
+                  <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="highlight"
+                    href="https://www.youtube.com/watch?v=mOm47gBMfg8"
+                  >
+                    Vitalik's TechTalk on
+                    zkRollup··································
+                    <span className="symboled"> ◎</span>
+                  </a>
+                </li>
+                <li>
+                  {' '}
+                  ➜{' '}
+                  <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="highlight"
+                    href="https://ethresear.ch/t/on-chain-scaling-to-potentially-500-tx-sec-through-mass-tx-validation/3477"
+                  >
+                    LibSNARK
+                    ································································
+                    <span className="symboled"> ◱</span>
+                  </a>
+                </li>
+
+                <li>
+                  {' '}
+                  ➜{' '}
+                  <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="highlight"
+                    href="https://ethresear.ch/t/on-chain-scaling-to-potentially-500-tx-sec-through-mass-tx-validation/3477"
+                  >
+                    EthSNARK ··················································
+                    <span className="symboled"> ◱</span>
+                  </a>
+                </li>
+
+                <li>
+                  {' '}
+                  ➜{' '}
+                  <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="highlight"
+                    href="https://ethresear.ch/t/on-chain-scaling-to-potentially-500-tx-sec-through-mass-tx-validation/3477"
+                  >
+                    Poseidon Hashing
+                    ·················································
+                    <span className="symboled"> ◱</span>
+                  </a>
+                </li>
+                <li>
+                  {' '}
+                  ➜{' '}
+                  <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="highlight"
+                    href="https://ethresear.ch/t/on-chain-scaling-to-potentially-500-tx-sec-through-mass-tx-validation/3477"
+                  >
+                    Quad-Merkle Tree
+                    ···························································
+                    <span className="symboled"> ◱</span>
+                  </a>
+                </li>
+
+                <li>
+                  {' '}
+                  ➜{' '}
+                  <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="highlight"
+                    href=""
+                  >
                     Loopring 3.0 smart contracts··············the on-chain part{' '}
                     <span className="symboled">⟁</span>
                   </a>
@@ -63,51 +148,17 @@ class SectionLoopringV30 extends React.Component {
                 <li>
                   {' '}
                   ➜{' '}
-                  <a className="highlight" href="">
+                  <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="highlight"
+                    href=""
+                  >
                     Loopring 3.0 ZKP circuits····················the off-chain
                     part <span className="symboled"> ⧉</span>
                   </a>
                 </li>
               </ul>
-            </div>
-
-            <div data-aos="fade-up" className="column is-5">
-              <h2>Zero-Knowledge Proofs Setup</h2>
-              <div className="content">
-                <p>
-                  Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
-                  Aenean commodo ligula eget dolor. Aenean massa. Cum sociis
-                  natoque penatibus et magnis dis parturient montes, nascetur
-                  ridiculus mus. Donec quam felis, ultricies nec, pellentesque
-                  eu, pretium quis, sem. Nulla consequat massa quis enim. Donec
-                  pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.
-                  In enim justo, rhoncus ut, imperdiet a, venenatis vitae,
-                  justo. Nullam dictum felis eu pede mollis pretium. Integer
-                  tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean
-                  vulputate eleifend tellus. Aenean leo ligula, porttitor eu,
-                  consequat vitae, eleifend ac, enim. Aliquam lorem ante,
-                  dapibus in, viverra quis, feugiat a,
-                </p>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
-                  Aenean commodo ligula eget dolor. Aenean massa. Cum sociis
-                  natoque penatibus et magnis dis parturient montes, nascetur
-                  ridiculus mus. Donec quam felis, ultricies nec, pellentesque
-                  eu, pretium quis, sem. Nulla consequat massa quis enim. Donec
-                  pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.
-                  In enim justo, rhoncus ut, imperdiet a, venenatis vitae,
-                  justo. Nullam dictum felis eu pede mollis pretium. Integer
-                  tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean
-                  vulputate eleifend tellus. Aenean leo ligula, porttitor eu,
-                  consequat vitae, eleifend ac, enim. Aliquam lorem ante,
-                  dapibus in, viverra quis, feugiat a,
-                </p>
-                <p>
-                  <a className="highlight" href="afdaf">
-                    Security Audit Report from SECBIT LABs↗
-                  </a>
-                </p>
-              </div>
             </div>
           </div>
         </div>
