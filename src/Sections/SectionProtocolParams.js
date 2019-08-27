7f76382a6714d57002c85f789f5232bb13a97cbe
Daniel Wang <daniel@loopring.org>
2019-08-27 11:49:06 -0400

    layout parameters

--- src/Sections/SectionProtocolParams.js
+++ src/Sections/SectionProtocolParams.js
@@ -1,31 +1,127 @@
 import React from 'react';
 import './SectionProtocolParams.scss';
-
-const API = './hacks_en.json';
+import '../../node_modules/aos/dist/aos.css';
+import AOS from 'aos';
+const API = './configs.json';
 
 class SectionProtocolParams extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      constants: [],
+      dexParams: [],
+      onchainParams: [],
+      offchainParams: [],
+    };
+    AOS.init();
+  }
+
+  componentDidMount() {
+    fetch(API)
+        .then((response) => response.json())
+        .then((data) =>
+          this.setState({
+            constants: data.constants,
+            dexParams: data.dexParams,
+            onchainParams: data.onchainParams,
+            offchainParams: data.offchainParams,
+          })
+        );
   }
 
   render() {
     return (
-      <section className="section protocol-section-3 is-large has-background-black">
-        <div className="container">
-          <div className="columns">
-            <div className="column is-5 has-text-left">
-              <h1>Configurations</h1>
-            </div>
-            <div className="column is-5 has-text-left"></div>
-          </div>
+      <section className="section protocol-section-params is-large has-background-black">
+        <div className="container has-text-centered">
+          <h1>DEX Configuration</h1>
 
-          <div className="columns">
+          <div className="columns is-centered">
             <div className="column is-5 has-text-left">
               <h2>Global Constants</h2>
+              <div>
+                <i>
+                  A list of protocol-wise constants that will never be changed.
+                </i>
+              </div>
+              <ul>
+                {this.state.constants.map((item, idx) => (
+                  <li key={idx}>
+                    <div className="has-leading-thin-arrow">
+                      <span className="name">{item.name}</span>{' '}
+                      <span className="tag">
+                        {item.type}: {item.value}
+                      </span>
+                    </div>
+                    <div className="meaning">{item.meaning}</div>
+                  </li>
+                ))}
+              </ul>
             </div>
 
             <div className="column is-5 has-text-left">
-              <h2>DEX Parameters</h2>
+              <h2>DEX Settings</h2>
+              <div>
+                <i>
+                  A list of DEX-wise constants whose values are fixed once the
+                  DEX is forged.
+                </i>
+              </div>
+              <ul>
+                {this.state.dexParams.map((item, idx) => (
+                  <li key={idx}>
+                    <div className="has-leading-thin-arrow">
+                      <span className="name">{item.name}</span>{' '}
+                      <span className="tag">
+                        {item.type}: {item.value}
+                      </span>
+                    </div>
+                    <div className="meaning">{item.meaning}</div>
+                  </li>
+                ))}
+              </ul>
+              <h2>DEX On-Chain Parameters</h2>
+              <div>
+                <i>
+                  A list of DEX-wise smart contract parameters whose values can
+                  only be changed by DEX owners.
+                </i>
+              </div>
+              <ul>
+                {this.state.onchainParams.map((item, idx) => (
+                  <li key={idx}>
+                    <div className="has-leading-thin-arrow">
+                      <span className="name">{item.name}</span>{' '}
+                      <span className="tag">
+                        {item.type}: {item.value}
+                      </span>
+                    </div>
+                    <div className="meaning">{item.meaning}</div>
+                  </li>
+                ))}
+              </ul>
+              <h2>DEX off-Chain Parameters</h2>
+              <div>
+                <i>
+                  A list of DEX-wise circuit parameters whose values can only be
+                  changed by DEX operators very frequently, e.g. per order
+                  and/or user request.
+                </i>
+              </div>
+              .
+              <ul>
+                {this.state.offchainParams.map((item, idx) => (
+                  <li key={idx}>
+                    <div className="has-leading-thin-arrow">
+                      <span className="name">{item.name}</span>{' '}
+                      <span className="tag">
+                        {item.type}: {item.value}
+                      </span>
+                    </div>
+                    <div className="meaning">{item.meaning}</div>
+                  </li>
+                ))}
+              </ul>
             </div>
           </div>
         </div>
