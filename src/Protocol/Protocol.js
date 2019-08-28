c68c1ba7acfe3a29d6524118d2aac243efeb3a4f
Daniel Wang <daniel@loopring.org>
2019-08-27 22:23:37 -0400

    rename

--- src/Protocol/Protocol.js
+++ src/Protocol/Protocol.js
@@ -9,9 +9,6 @@ import PerformanceSection from './PerformanceSection.js';
 import DesignSection from './DesignSection.js';
 import ConfigurationSection from './ConfigurationSection.js';
 
-// import {Tab, Tabs, TabList, TabPanel} from 'react-tabs';
-// import 'react-tabs/style/react-tabs.css';
-
 class Protocol extends React.Component {
   render() {
     return (
