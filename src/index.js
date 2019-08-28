eff3b0d37da993731b6320e7e55d4456eea986f0
Daniel Wang <daniel@loopring.org>
2019-08-27 22:47:01 -0400

    rename

--- src/index.js
+++ src/index.js
@@ -26,6 +26,8 @@ const routing = (
     </ScrollToTop>
   </Router>
 );
+
+console.log('public url: ', process.env.PUBLIC_URL);
 ReactDOM.render(routing, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
