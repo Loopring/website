ad24c72aab4e49684fc64e9002f5608bf121c7d0
Daniel Wang <daniel@loopring.org>
2019-08-29 13:11:05 -0400

    minor

--- src/LRC/StakingSection.js
+++ src/LRC/StakingSection.js
@@ -1,14 +1,12 @@
 import React from 'react';
 
-import {withLocalize} from 'react-localize-redux';
-import {Translate} from 'react-localize-redux';
 import englishTranslation from './i18n/StakingSection.en.json';
 import chineseTranslation from './i18n/StakingSection.zh.json';
-import I18nComponent from '../Components/I18nComponent';
+import JsonI18nComponent from '../Components/JsonI18nComponent';
 
 import './StakingSection.scss';
 
-class StakingSection extends I18nComponent {
+class StakingSection extends JsonI18nComponent {
   constructor(props) {
     super(props, englishTranslation, chineseTranslation);
   }
@@ -19,88 +17,32 @@ class StakingSection extends I18nComponent {
         <div className="container is-centered has-text-centered">
           <div className="columns is-centered">
             <div data-aos="fade-up" className="column is-10 has-text-centered">
-              <h1>
-                <Translate id="headingStaking" />
-              </h1>
+              <h1>{this.state.I.heading}</h1>
               {/* Unlike many other projects, Loopring's utility token LRC is no longer a fee token.*/}
             </div>
           </div>
-          {/* ---*/}
-          <div className="columns is-centered is-multiline">
-            <div
-              data-aos="fade-up"
-              className="column  is-2 has-text-right has-text-centered-mobile"
-            >
-              <div className="content">
-                <div className="category is-unselectable">☉</div>
+
+          {this.state.I.utilities.map((item, idx) => (
+            <div key="idx" className="columns is-centered">
+              <div
+                data-aos="fade-up"
+                className="column  is-2 has-text-right has-text-centered-mobile"
+              >
+                <div className="content">
+                  <div className="category is-unselectable">{item.symbol}</div>
+                </div>
+              </div>
+
+              <div data-aos="fade-up" className="column is-4  has-text-left">
+                <h2>{item.title}</h2>
+                <p className="has-text-justify">{item.detail}</p>
               </div>
             </div>
-
-            <div data-aos="fade-up" className="column is-4  has-text-left">
-              <h2>1/ Earn Protocol Fees, by Holders</h2>
-              <p className="has-text-justify">
-                Every LRC holder can stake LRC to earn part of 70% of the
-                protocol fees of all exchanges built on top of Loopring — 20%
-                will fund the Loopring DAO and the remaining 10% will be burned.
-              </p>
-            </div>
-          </div>
-          <div className="columns is-centered">
-            <div
-              data-aos="fade-up"
-              className="column  is-2 has-text-right has-text-centered-mobile"
-            >
-              <div className="category is-unselectable">☐</div>
-            </div>
-
-            <div data-aos="fade-up" className="column is-4  has-text-left">
-              <h2>2/ Build up DEX Reputation, by DEX Owners</h2>
-              <p className="has-text-justify">
-                An exchange owner needs to stake LRC for economic security and
-                builing up its reputation.
-              </p>
-            </div>
-          </div>
-          <div className="columns is-centered">
-            <div
-              data-aos="fade-up"
-              className="column  is-2 has-text-right has-text-centered-mobile"
-            >
-              <div className="category is-unselectable">⟁</div>
-            </div>
-
-            <div data-aos="fade-up" className="column is-4  has-text-left">
-              <div className="content">
-                <h2>3/ Lower Protocol Fees, by DEX Owners</h2>
-                <p className="has-text-justify">
-                  LRC can be staked to lower the protocol fees on a specific
-                  exchange by an exchange owner, market makers and
-                  high-frequency traders.
-                </p>
-              </div>
-            </div>
-          </div>
-          <div className="columns is-centered">
-            <div
-              data-aos="fade-up"
-              className="column  is-2 has-text-right has-text-centered-mobile"
-            >
-              <div className="category is-unselectable">∖</div>
-            </div>
-
-            <div data-aos="fade-up" className="column is-4  has-text-left">
-              <h2>4/ Lower Protocol Fees, by Traders</h2>
-              <p className="has-text-justify">
-                LRC can be staked to lower the protocol fees on a specific
-                exchange by an exchange owner, market makers and high-frequency
-                traders.
-              </p>
-            </div>
-          </div>
+          ))}
         </div>
       </section>
     );
   }
 }
 
-export default withLocalize(StakingSection);
+export default StakingSection;
