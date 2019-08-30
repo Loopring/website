043122e07fb751a24fbe95c24ff1322bf00378da
Daniel Wang <daniel@loopring.org>
2019-08-30 00:27:22 -0400

    integrate posts

--- src/Updates/Updates.js
+++ src/Updates/Updates.js
@@ -6,7 +6,12 @@ import Footer from "../Components/Footer";
 import SectionUpdatesHero from "./HeroSection";
 import ArticlesSection from "./ArticlesSection";
 
+import GhostContentAPI from "@tryghost/content-api";
+
 import "./Updates.scss";
+
+const API =
+  "https://loopring.ghost.io/ghost/api/v2/content/posts/?key=b19ddb3d08ecbf3eed7c737e17&page=1&fields=title,url,published_at,custom_excerpt,featured,feature_image";
 
 class Updates extends React.Component {
   render() {
