11ef11901b2d748e2eb786a3e062262e9bfd2ea2
Daniel Wang <daniel@loopring.org>
2019-08-29 23:19:04 -0400

    better language detection

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, HashRouter } from "react-router-dom";
 import ScrollToTop from "./Components/ScrollToTop";
 import { LocalizeProvider, withLocalize } from "react-localize-redux";
+import detectBrowserLanguage from "detect-browser-language";
 
 import About from "./About/About";
 import Protocol from "./Protocol/Protocol";
@@ -12,6 +13,10 @@ import NotFound from "./NotFound";
 
 class App extends React.Component {
   render() {
+    const lang = detectBrowserLanguage().toLowerCase();
+    if (lang === "zh" || lang == "zh-cn") {
+      document.title = "路印 - 高性能去中心化交易协议";
+    }
     return (
       <LocalizeProvider>
         <HashRouter>
