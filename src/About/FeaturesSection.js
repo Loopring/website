5d7245a369a2bb26077260c9a114b0eaef3bff11
Daniel Wang <daniel@loopring.org>
2019-08-29 20:06:49 -0400

    more i18n for protocol page

--- src/About/FeaturesSection.js
+++ src/About/FeaturesSection.js
@@ -28,42 +28,68 @@ class FeaturesSection extends I18nComponent {
           <div className="columns is-multiline">
             <div data-aos="fade-up" className="column is-2 is-offset-2">
               <span className="has-leading-thick-arrow deprecated">
-                Loopring 1.0 (deprecated)
-              </span>{" "}
+                <Translate id="loopring" /> 1.0 <Translate id="deprecated" />
+              </span>
               ······
             </div>
             <div data-aos="fade-up" className="column column-features is-6">
-              <span className="tag highlight">Ring-Matching</span>
-              <span className="tag highlight">Order-Sharing</span>
-              <span className="tag highlight">Non-Custodial</span>
+              <span className="tag highlight">
+                <Translate id="features.ringMatching" />
+              </span>
+              <span className="tag highlight">
+                <Translate id="features.orderSharing" />
+              </span>
+              <span className="tag highlight">
+                <Translate id="features.nonCustodial" />
+              </span>
             </div>
 
             <div data-aos="fade-up" className="column is-2 is-offset-2">
               <span className="has-leading-thick-arrow deprecated">
-                Loopring 1.5 (deprecated)
-              </span>{" "}
+                <Translate id="loopring" /> 1.5 <Translate id="deprecated" />
+              </span>
               ······
             </div>
             <div data-aos="fade-up" className="column column-features is-6">
-              <span className="tag">Ring-Matching</span>
-              <span className="tag ">Order-Sharing</span>
-              <span className="tag">Non-Custodial</span>
-              <span className="tag highlight">Dual-Authoring</span>
+              <span className="tag">
+                <Translate id="features.ringMatching" />
+              </span>
+              <span className="tag ">
+                <Translate id="features.orderSharing" />
+              </span>
+              <span className="tag">
+                <Translate id="features.nonCustodial" />
+              </span>
+              <span className="tag highlight">
+                <Translate id="features.dualAuthoring" />
+              </span>
             </div>
 
             <div data-aos="fade-up" className="column is-2 is-offset-2">
               <span className="has-leading-thick-arrow deprecated">
-                Loopring 2.0 (deprecated)
-              </span>{" "}
+                <Translate id="loopring" /> 2.0 <Translate id="deprecated" />
+              </span>
               ······
             </div>
             <div data-aos="fade-up" className="column column-features is-6">
-              <span className="tag">Ring-Matching</span>
-              <span className="tag">Order-Sharing</span>
-              <span className="tag">Non-Custodial</span>
-              <span className="tag">Dual-Authoring</span>
-              <span className="tag highlight">Any-Token-As-Fee</span>
-              <span className="tag highlight">LRC-Burning</span>
+              <span className="tag">
+                <Translate id="features.ringMatching" />
+              </span>
+              <span className="tag">
+                <Translate id="features.orderSharing" />
+              </span>
+              <span className="tag">
+                <Translate id="features.nonCustodial" />
+              </span>
+              <span className="tag">
+                <Translate id="features.dualAuthoring" />
+              </span>
+              <span className="tag highlight">
+                <Translate id="features.anyTokenAsFee" />
+              </span>
+              <span className="tag highlight">
+                <Translate id="features.lrcBurning" />
+              </span>
             </div>
 
             <div data-aos="fade-up" className="column is-2 is-offset-2">
@@ -71,19 +97,43 @@ class FeaturesSection extends I18nComponent {
               ···························
             </div>
             <div data-aos="fade-up" className="column column-features is-6">
-              <span className="tag deprecated">Ring-Matching</span>
-              <span className="tag deprecated">Order-Sharing</span>
-              <span className="tag deprecated">Non-Custodial</span>
-              <span className="tag deprecated">Dual-Authoring</span>
-              <span className="tag deprecated">Any-Token-as-Fee</span>
-              <span className="tag">LRC-Burning</span>
-              <span className="tag highlight">Custodial-in-Contract</span>
-              <span className="tag highlight">Trade-Token-as-Fee</span>
+              <span className="tag deprecated">
+                <Translate id="features.ringMatching" />
+              </span>
+              <span className="tag deprecated">
+                <Translate id="features.orderSharing" />
+              </span>
+              <span className="tag deprecated">
+                <Translate id="features.nonCustodial" />
+              </span>
+              <span className="tag deprecated">
+                <Translate id="features.dualAuthoring" />
+              </span>
+              <span className="tag deprecated">
+                <Translate id="features.anyTokenAsFee" />
+              </span>
+              <span className="tag">
+                <Translate id="features.lrcBurning" />
+              </span>
+              <span className="tag highlight">
+                <Translate id="features.custodialInContract" />
+              </span>
+              <span className="tag highlight">
+                <Translate id="features.tradeTokenAsFee" />
+              </span>
               <span className="tag highlight">zkRollup</span>
-              <span className="tag highlight">LRC-Staking</span>
-              <span className="tag highlight">High-Throughtput</span>
-              <span className="tag highlight">Low-Cost</span>
-              <span className="tag highlight">Upgradability</span>
+              <span className="tag highlight">
+                <Translate id="features.lrcStaking" />
+              </span>
+              <span className="tag highlight">
+                <Translate id="features.highThroughput" />
+              </span>
+              <span className="tag highlight">
+                <Translate id="features.lowCost" />
+              </span>
+              <span className="tag highlight">
+                <Translate id="features.upgradability" />
+              </span>
             </div>
           </div>
         </div>
