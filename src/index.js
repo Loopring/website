ce7c450939945de07fb7e24c589a8d399a9d1c72
Daniel Wang <daniel@loopring.org>
2019-08-29 23:20:02 -0400

    better language detection

--- src/index.js
+++ src/index.js
@@ -5,8 +5,6 @@ import * as serviceWorker from "./serviceWorker";
 import "./index.scss";
 
 console.log("public url: ", process.env.PUBLIC_URL);
-console.log("navigator.browserLanguage: ", navigator.browserLanguage);
-console.log("navigator.language: ", navigator.language);
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
