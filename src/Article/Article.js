65e8b10ff6c2c94b8f049ffb62a652f514471c5e
Daniel Wang <daniel@loopring.org>
2019-09-01 02:33:35 -0400

    fix navbar in mobile

--- src/Article/Article.js
+++ src/Article/Article.js
@@ -6,6 +6,8 @@ import Header from "../Components/Header.js";
 import Footer from "../Components/Footer.js";
 
 import GhostContentAPI from "@tryghost/content-api";
+import Moment from "react-moment";
+import "moment-timezone";
 // import ContentLoader, { Facebook } from "react-content-loader";
 
 const api = new GhostContentAPI({
@@ -13,6 +15,15 @@ const api = new GhostContentAPI({
   key: "b19ddb3d08ecbf3eed7c737e17",
   version: "v2"
 });
+
+const calendarStrings = {
+  lastDay: "[Yesterday] LT",
+  sameDay: "[Today] LT",
+  nextDay: "[Tomorrow] LT",
+  lastWeek: "[last] dddd [at] LT",
+  nextWeek: "dddd [at] LT",
+  sameElse: "L"
+};
 
 class Article extends React.Component {
   constructor(props) {
@@ -36,7 +47,8 @@ class Article extends React.Component {
 
     api.posts
       .read({
-        slug: params.postId
+        slug: params.postId,
+        include: "tags"
       })
       .then(post => {
         this.setState({ post: post });
@@ -52,21 +64,50 @@ class Article extends React.Component {
     const {
       match: { params }
     } = this.props;
+    console.log(this.state.post);
     const html = this.trimHtml(this.state.post.html);
+    const tags = this.state.post.tags ? this.state.post.tags : [];
+    const publishedAt = this.state.post.published_at
+      ? this.state.post.published_at
+      : "";
+
     return (
       <div>
         <Header />
+        <section className="section  has-background-white">
+          <div className="container">
+            <div className="columns is-centered">
+              <div className="column is-6 has-text-left">
+                <h1>{this.state.post.title}</h1>
+                <div>
+                  {" "}
+                  <Moment
+                    className="datetime"
+                    lparse="YYYY-MM-DD HH:mm"
+                    calendar={calendarStrings}
+                  >
+                    {publishedAt}
+                  </Moment>
+                  {tags.map((tag, idx) => (
+                    <span className="tag" key="idx">
+                      {tag.name}
+                    </span>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+
         <section className="section section-article-hero-image">
-          <figure data-aos="fade-up" className="image">
+          <figure data-aos="fade-zoom-in" className="image">
             {<img draggable="false" src={this.state.post.feature_image} />}
           </figure>
         </section>
         <section className="section section-article is-small has-background-white">
           <div className="container">
             <div className="columns is-centered">
-              <div className="column is-5">
-                <h1>{this.state.post.title}</h1>
-
+              <div className="column is-6">
                 <div
                   className="postWrapper"
                   dangerouslySetInnerHTML={{ __html: html }}
