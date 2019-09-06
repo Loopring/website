99191ed304e0737b79873821591d44817c327ab9
Daniel Wang <daniel@loopring.org>
2019-09-06 17:00:49 -0400

    add translation

--- src/About/HeroSection.js
+++ src/About/HeroSection.js
@@ -23,15 +23,28 @@ class HeroSection extends I18nComponent {
             <defs>
               <mask id="mask" x="0" y="0" width="100%" height="100%">
                 <rect id="alpha" x="0" y="0" width="100%" height="100%" />
-                <text
-                  className="has-text-din"
-                  id="title"
-                  x="50%"
-                  y="35%"
-                  dy="1.58em"
-                >
-                  <Translate id="loopring" />
-                </text>
+                {this.isChinese() ? (
+                  <text
+                    className="chinese"
+                    id="title"
+                    x="50%"
+                    y="35%"
+                    dy="1.58em"
+                  >
+                    路印协议
+                  </text>
+                ) : (
+                  <text
+                    className="has-text-din"
+                    id="title"
+                    x="50%"
+                    y="35%"
+                    dy="1.58em"
+                  >
+                    Loopring
+                  </text>
+                )}
+
                 <text id="subtitle" x="50%" y="32%" dy="9.8em">
                   <Translate id="slogan" />
                 </text>
