8711e892a7c1815632bc8511f5498229c77742cd
Daniel Wang <daniel@loopring.org>
2019-10-29 00:08:11 -0400

    more

--- src/Ceremony/Participant.js
+++ src/Ceremony/Participant.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import "./Participant.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -6,42 +6,77 @@ import { faCheckCircle } from "@fortawesome/free-solid-svg-icons";
 import { faClock } from "@fortawesome/free-solid-svg-icons";
 import { faSync } from "@fortawesome/free-solid-svg-icons";
 
-const Participant = ({ name, org, hash, status }) => {
-  let icon = faClock;
-
-  switch (status) {
-    case "done":
-      icon = faCheckCircle;
-      break;
-    case "running":
-      icon = faSync;
-      break;
-    default:
+class Participant extends React.Component {
+  randomHash() {
+    const HASH_SIZE = 48;
+    let result = "";
+    const characters =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    const charactersLength = characters.length;
+    for (let i = 0; i < HASH_SIZE; i++) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
   }
 
-  return (
-    <div
-      className={
-        status + " columns  setup-participant  is-centered is-multiline"
-      }
-    >
-      <div className="column is-1 has-text-left ">{name}</div>
+  constructor(props) {
+    super(props);
+    this.state = {
+      hash: this.props.hash,
+      isRunning: this.props.status === "running"
+    };
 
-      <div className="column is-1 has-text-left  ">{org}</div>
+    if (this.state.isRunning) {
+      this.refresh = setInterval(() => {
+        this.setState({ hash: this.randomHash() });
+      }, 200);
+    }
+  }
 
-      <div className="column is-8 is-vcentered ">
-        <span className="hash">{hash}</span>
+  componentWillUnmount() {
+    if (this.refresh) {
+      clearInterval(this.refresh);
+    }
+  }
+
+  render() {
+    let icon = faClock;
+
+    switch (this.props.status) {
+      case "done":
+        icon = faCheckCircle;
+        break;
+      case "running":
+        icon = faSync;
+        break;
+      default:
+    }
+
+    return (
+      <div
+        className={
+          this.props.status +
+          " columns  setup-participant  is-centered is-multiline"
+        }
+      >
+        <div className="column is-1 has-text-left">{this.props.name}</div>
+
+        <div className="column is-1 has-text-left">{this.props.org}</div>
+
+        <div className="column is-8 is-vcentered">
+          <span className="hash">{this.state.hash}</span>
+        </div>
+        <div className="column is-1">
+          <FontAwesomeIcon
+            className="icon"
+            spin={this.state.isRunning}
+            icon={icon}
+            size="1x"
+          />
+        </div>
       </div>
-      <div className="column is-1">
-        <FontAwesomeIcon
-          className="icon"
-          spin={status === "running"}
-          icon={icon}
-          size="1x"
-        />
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Participant;
