eafeb8c503c4ae8e5d1280d34d70860a8b29b6ff
Daniel Wang <daniel@loopring.org>
2019-11-09 22:52:55 -0500

    update ceremony

--- src/Team/CeremonySection.js
+++ src/Team/CeremonySection.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-import englishTranslation from "./i18n/CeremonySection.en.json";
-import chineseTranslation from "./i18n/CeremonySection.zh.json";
+import englishTranslation from "../Ceremony/i18n/Ceremony.en.json";
+import chineseTranslation from "../Ceremony/i18n/Ceremony.zh.json";
 import JsonI18nComponent from "../Components/JsonI18nComponent";
 
 import "./CeremonySection.scss";
@@ -9,39 +9,26 @@ import "./CeremonySection.scss";
 class CeremonySection extends JsonI18nComponent {
   constructor(props) {
     super(props, englishTranslation, chineseTranslation);
-    this.state.participants = [];
-  }
-
-  componentDidMount() {
-    const urlBase =
-      "https://raw.githubusercontent.com/Loopring/website/master/src/Ceremony/i18n/";
-    const url = this.isChinese()
-      ? urlBase + "Ceremony.zh.json"
-      : urlBase + "Ceremony.en.json";
-
-    fetch(url)
-      .then(response => response.json())
-      .then(data => this.setState({ participants: data.done }));
   }
 
   render() {
-    return this.state.participants.length === 0 ? (
+    return this.state.I.participants.done.length === 0 ? (
       <span />
     ) : (
-      <section className="section section-ceremony is-large  has-background-white">
+      <section className="section section-ceremony is-large  has-background-primary-dark">
         <div className="container">
           <div className="columns ">
             <div
               data-aos="fade-up"
               className="column is-5 has-text-centered has-text-right-tablet"
             >
-              <h1>{this.state.I.heading1}</h1>
-              <h1>{this.state.I.heading2}</h1>
-              <h1>{this.state.I.heading3}</h1>
+              <h1>{this.state.I.teamPage.heading1}</h1>
+              <h1>{this.state.I.teamPage.heading2}</h1>
+              <h1>{this.state.I.teamPage.heading3}</h1>
             </div>
             <div data-aos="fade-up" className="column is-7">
               <ul>
-                {this.state.participants.map((participant, idx) => (
+                {this.state.I.participants.done.map((participant, idx) => (
                   <li key={idx}>
                     <a
                       className="highlight has-leading-thin-arrow"
