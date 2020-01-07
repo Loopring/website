81eae77728512b0f1283fb0ef9d2ef5e3d59e7f6
Daniel Wang <daniel@loopring.org>
2020-01-08 07:54:29 +0800

    Subscribe (#75)

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -4,24 +4,69 @@ import { withLocalize } from "react-localize-redux";
 import { Translate } from "react-localize-redux";
 import englishTranslation from "./i18n/Footer.en.json";
 import chineseTranslation from "./i18n/Footer.zh.json";
-import I18nComponent from "./I18nComponent";
+import JsonI18nComponent from "./JsonI18nComponent";
+import axios from "axios";
 
 import "./Footer.scss";
 
-class Footer extends I18nComponent {
+class Footer extends JsonI18nComponent {
   constructor(props) {
     super(props, englishTranslation, chineseTranslation);
+  }
+  subscribe(e) {
+    const url = "https://api.convertkit.com/v3/forms/1156897/subscribe";
+    const apiKey = "qoOCFRB819dkXMM59a7M0Q";
+    // this.burgerRef.current.classList.toggle("is-active");
+    // this.menuRef.current.classList.toggle("is-active");
+
+    axios
+      .post(
+        url,
+        { api_key: apiKey, email: "test@google.com" },
+        { headers: { "Content-Type": "application/json" } }
+      )
+      .then(function(response) {
+        console.log(response);
+      });
   }
 
   render() {
     return (
       <footer className="footer is-centered has-background-white">
         <div className="container is-centered has-text-centered">
+          {/* <div className="columns is-centered is-vcentered">
+            <div className="column is-4  is-right is-vcentered has-text-centered">
+              <div className="field">
+                <div className="control has-icons-left has-icons-right">
+                  <input
+                    className="input"
+                    type="email"
+                    placeholder={this.state.I.yourEmail}
+                  />
+                  <span className="icon is-small is-left">
+                    <i className="fas fa-envelope"></i>
+                  </span>
+                  <span className="icon is-small is-right">
+                    <i className="fas fa-exclamation-triangle"></i>
+                  </span>
+                </div>
+              </div>
+
+              <div className="field">
+                <div className="control">
+                  <button
+                    className="button is-link"
+                    onClick={this.subscribe.bind(this)}
+                  >
+                    {this.state.I.subscribe}
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>*/}
           <ul>
             <li>
-              <a href="mailto:foundation@loopring.org">
-                <Translate id="email" />
-              </a>
+              <a href="mailto:foundation@loopring.org">{this.state.I.email}</a>
             </li>
             ⭑
             <li>
@@ -42,13 +87,13 @@ class Footer extends I18nComponent {
             ⭑
             <li>
               <a href="https://twitter.com/loopringorg">
-                <Translate id="twitter" />
+                {this.state.I.twitter}
               </a>
             </li>
             ⭑
             <li>
               <a href="https://weibo.com/loopringfoundation">
-                <Translate id="weibo" />
+                {this.state.I.weibo}
               </a>
             </li>
             ⭑
@@ -86,10 +131,7 @@ class Footer extends I18nComponent {
           </ul>
           <div className="org">
             <p className="has-text-grey">
-              <Translate id="org" />
-            </p>
-            <p className="wechat">
-              <Translate id="wechat" />
+              {this.state.I.org} {this.state.I.wechat}
             </p>
           </div>
         </div>
