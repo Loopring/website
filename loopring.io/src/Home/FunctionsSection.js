6642c9475b83749810d5ce34a8097047d5bfc309
wangdong <wangdong@zhongan.io>
2020-11-08 21:34:03 +0800

    more

--- loopring.io/src/Home/FunctionsSection.js
+++ loopring.io/src/Home/FunctionsSection.js
@@ -17,62 +17,29 @@ class FunctionsSection extends JsonI18nComponent {
   render() {
     return (
       <div>
-        <section className="section section-functions is-large has-background-blue">
+        <section className="section section-functions is-large has-background-white">
           <div className="container">
             <div className="columns is-multiline is-centered is-vcentered">
               <div
                 data-aos="fade-up"
-                className="column is-12 is-centered is-vcentered has-text-centered"
+                className="column is-6 is-centered is-vcentered has-text-centered"
               >
-                <h1 className="build-title">{this.state.I.heading}</h1>
-              </div>
-              <div className="column is-12 is-centered is-vcentered has-text-centered">
-                <div className="outer-container">
-                  <div
-                    data-aos="fade-up"
-                    className="inner-container has-text-centered"
-                  >
-                    <h2>
-                      <span className="heading-number">1 /</span>{" "}
-                      {this.state.I.option1.title}
-                    </h2>
-                    <div className="details has-text-left">
-                      <p
-                        dangerouslySetInnerHTML={{
-                          __html: this.state.I.option1.details
-                        }}
-                      />
-                    </div>
-                    <a
-                      href="https://github.com/Loopring/protocols/blob/master/packages/loopring_v3/BACKEND.md"
-                      className="button is-outlined left"
-                    >
-                      {this.state.I.option1.action}
-                    </a>
-                  </div>
-                  <div
-                    data-aos="fade-up"
-                    className="inner-container  has-text-centered right"
-                  >
-                    <h2>
-                      <span className="heading-number">2 /</span>{" "}
-                      {this.state.I.option2.title}
-                    </h2>
-                    <div className="details has-text-left">
-                      <p
-                        dangerouslySetInnerHTML={{
-                          __html: this.state.I.option2.details
-                        }}
-                      />
-                    </div>
-                    <a
-                      href="mailto:exchange@loopring.io"
-                      className="button is-outlined right"
-                    >
-                      {this.state.I.option2.action}
-                    </a>
-                  </div>
+                <div className="has-text-left">
+                  <h1>守护人</h1>
+                  <h2>恢复 I 锁定/解锁 I 审批超额转账</h2>
+                  <p>
+                    守护人必须是您绝对信任的人。只要超过一半的守护人同意就能决定钱包资产的最终拥有权。
+                  </p>
+                  <p>我们也支持非路印钱包做守护人。</p>
                 </div>
+              </div>
+              <div
+                data-aos="fade-up"
+                className="column is-6 is-centered is-vcentered has-text-left"
+              >
+                <figure className="image">
+                  <img draggable="false" src={"/images/feature_B1.jpg"} />
+                </figure>
               </div>
             </div>
           </div>
