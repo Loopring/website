eb83219082ebc906e439ae6850b85e5a267b3c2d
mfinestone <36642873+mfinestone@users.noreply.github.com>
2021-04-02 00:38:58 -0400

    edit

--- loopring.io/src/Wallet_en/HeroSection.js
+++ loopring.io/src/Wallet_en/HeroSection.js
@@ -137,8 +137,10 @@ const HeroSection = (props) => {
               orderbook trading, AMMs, and global payments, right from the
               Loopring wallet.
             </div>
+             <div
                Already have a different wallet and just want to use Loopring L2 on web?
                Head to Loopring Exchange and connect.
+            </div>
           </div>
         </CenterAlignContainer>
       </Section>
