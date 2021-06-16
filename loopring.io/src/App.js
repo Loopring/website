571c64b61bd56c1ddb03e50d3f54fe90dfb4a506
Daniel Wang <dong77@gmail.com>
2021-06-16 12:07:11 +0800

    force HTTPS for loopring.io

--- loopring.io/src/App.js
+++ loopring.io/src/App.js
@@ -3,7 +3,7 @@ import {Route, Redirect, Switch, HashRouter} from 'react-router-dom';
 import ScrollToTop from './Components/ScrollToTop';
 import {LocalizeProvider, withLocalize} from 'react-localize-redux';
 import detectBrowserLanguage from 'detect-browser-language';
-
+import HttpsRedirect from 'react-https-redirect';
 import NotFound from './NotFound/NotFound';
 import RedPacket from './RedPacket/RedPacket';
 import StakingClaim from './StakingClaim/StakingClaim';
@@ -21,222 +21,227 @@ class App extends React.Component {
     const isChinese = lang === 'zh' || lang === 'zh-cn';
     document.title = isChinese ? '路印科技 - 钱包' : 'Loopring - Wallet';
     return (
-      <LocalizeProvider>
-        <HashRouter>
-          <ScrollToTop>
-            <Switch>
-              <Route
-                path="/"
-                exact
-                component={isChinese ? Walletzh : Walleten}
-              />
-              <Route
-                path="/redpacket"
-                exact
-                render={(props) => <RedPacket isChinese={isChinese} />}
-              />
+      <HttpsRedirect>
+        <LocalizeProvider>
+          <HashRouter>
+            <ScrollToTop>
+              <Switch>
+                <Route
+                  path="/"
+                  exact
+                  component={isChinese ? Walletzh : Walleten}
+                />
+                <Route
+                  path="/redpacket"
+                  exact
+                  render={(props) => <RedPacket isChinese={isChinese} />}
+                />
 
-              <Route
-                path="/staking"
-                exact
-                render={(props) => <StakingClaim isChinese={isChinese} />}
-              />
+                <Route
+                  path="/staking"
+                  exact
+                  render={(props) => <StakingClaim isChinese={isChinese} />}
+                />
 
-              <Route path="/wmining" exact component={WMiningzh} />
-              <Route
-                path="/legal/terms/en"
-                exact
-                render={(props) => (
-                  <SimpleMarkdownPage name="terms" isChinese={false} />
-                )}
-              />
-              <Route
-                path="/legal/terms/zh"
-                exact
-                render={(props) => (
-                  <SimpleMarkdownPage name="terms" isChinese={true} />
-                )}
-              />
-              <Route
-                path="/legal/terms"
-                exact
-                render={(props) => (
-                  <SimpleMarkdownPage name="terms" isChinese={isChinese} />
-                )}
-              />
-              <Route
-                path="/legal/privacy/en"
-                exact
-                render={(props) => (
-                  <SimpleMarkdownPage name="privacy" isChinese={false} />
-                )}
-              />
-              <Route
-                path="/legal/privacy/zh"
-                exact
-                render={(props) => (
-                  <SimpleMarkdownPage name="privacy" isChinese={true} />
-                )}
-              />
-              <Route
-                path="/legal/privacy"
-                exact
-                render={(props) => (
-                  <SimpleMarkdownPage name="privacy" isChinese={isChinese} />
-                )}
-              />
+                <Route path="/wmining" exact component={WMiningzh} />
+                <Route
+                  path="/legal/terms/en"
+                  exact
+                  render={(props) => (
+                    <SimpleMarkdownPage name="terms" isChinese={false} />
+                  )}
+                />
+                <Route
+                  path="/legal/terms/zh"
+                  exact
+                  render={(props) => (
+                    <SimpleMarkdownPage name="terms" isChinese={true} />
+                  )}
+                />
+                <Route
+                  path="/legal/terms"
+                  exact
+                  render={(props) => (
+                    <SimpleMarkdownPage name="terms" isChinese={isChinese} />
+                  )}
+                />
+                <Route
+                  path="/legal/privacy/en"
+                  exact
+                  render={(props) => (
+                    <SimpleMarkdownPage name="privacy" isChinese={false} />
+                  )}
+                />
+                <Route
+                  path="/legal/privacy/zh"
+                  exact
+                  render={(props) => (
+                    <SimpleMarkdownPage name="privacy" isChinese={true} />
+                  )}
+                />
+                <Route
+                  path="/legal/privacy"
+                  exact
+                  render={(props) => (
+                    <SimpleMarkdownPage name="privacy" isChinese={isChinese} />
+                  )}
+                />
 
-              <Route
-                path="/legal/risks/en"
-                exact
-                render={(props) => (
-                  <SimpleMarkdownPage name="risks" isChinese={false} />
-                )}
-              />
-              <Route
-                path="/legal/risks/zh"
-                exact
-                render={(props) => (
-                  <SimpleMarkdownPage name="risks" isChinese={true} />
-                )}
-              />
-              <Route
-                path="/legal/risks"
-                exact
-                render={(props) => (
-                  <SimpleMarkdownPage name="risks" isChinese={isChinese} />
-                )}
-              />
+                <Route
+                  path="/legal/risks/en"
+                  exact
+                  render={(props) => (
+                    <SimpleMarkdownPage name="risks" isChinese={false} />
+                  )}
+                />
+                <Route
+                  path="/legal/risks/zh"
+                  exact
+                  render={(props) => (
+                    <SimpleMarkdownPage name="risks" isChinese={true} />
+                  )}
+                />
+                <Route
+                  path="/legal/risks"
+                  exact
+                  render={(props) => (
+                    <SimpleMarkdownPage name="risks" isChinese={isChinese} />
+                  )}
+                />
 
-              <Route
-                path="/legal/walletdesign/en"
-                exact
-                render={(props) => (
-                  <SimpleMarkdownPage name="walletdesign" isChinese={false} />
-                )}
-              />
-              <Route
-                path="/legal/walletdesign/zh"
-                exact
-                render={(props) => (
-                  <SimpleMarkdownPage name="walletdesign" isChinese={true} />
-                )}
-              />
-              <Route
-                path="/legal/walletdesign"
-                exact
-                render={(props) => (
-                  <SimpleMarkdownPage
-                    name="walletdesign"
-                    isChinese={isChinese}
+                <Route
+                  path="/legal/walletdesign/en"
+                  exact
+                  render={(props) => (
+                    <SimpleMarkdownPage name="walletdesign" isChinese={false} />
+                  )}
+                />
+                <Route
+                  path="/legal/walletdesign/zh"
+                  exact
+                  render={(props) => (
+                    <SimpleMarkdownPage name="walletdesign" isChinese={true} />
+                  )}
+                />
+                <Route
+                  path="/legal/walletdesign"
+                  exact
+                  render={(props) => (
+                    <SimpleMarkdownPage
+                      name="walletdesign"
+                      isChinese={isChinese}
+                    />
+                  )}
+                />
+
+                <Route
+                  path="/legal/contracts/en"
+                  exact
+                  render={(props) => (
+                    <SimpleMarkdownPage name="contracts" isChinese={false} />
+                  )}
+                />
+                <Route
+                  path="/legal/contracts/zh"
+                  exact
+                  render={(props) => (
+                    <SimpleMarkdownPage name="contracts" isChinese={true} />
+                  )}
+                />
+                <Route
+                  path="/legal/contracts"
+                  exact
+                  render={(props) => (
+                    <SimpleMarkdownPage
+                      name="contracts"
+                      isChinese={isChinese}
+                    />
+                  )}
+                />
+
+                <Route
+                  path="/legal/aboutl2/en"
+                  exact
+                  render={(props) => (
+                    <SimpleMarkdownPage name="aboutl2" isChinese={false} />
+                  )}
+                />
+                <Route
+                  path="/legal/aboutl2/zh"
+                  exact
+                  render={(props) => (
+                    <SimpleMarkdownPage name="aboutl2" isChinese={true} />
+                  )}
+                />
+                <Route
+                  path="/legal/aboutl2"
+                  exact
+                  render={(props) => (
+                    <SimpleMarkdownPage name="aboutl2" isChinese={isChinese} />
+                  )}
+                />
+
+                <Route
+                  path="/legal/points/en"
+                  exact
+                  render={(props) => (
+                    <SimpleMarkdownPage name="points" isChinese={false} />
+                  )}
+                />
+                <Route
+                  path="/legal/points/zh"
+                  exact
+                  render={(props) => (
+                    <SimpleMarkdownPage name="points" isChinese={true} />
+                  )}
+                />
+                <Route
+                  path="/legal/points"
+                  exact
+                  render={(props) => (
+                    <SimpleMarkdownPage name="points" isChinese={isChinese} />
+                  )}
+                />
+
+                <Route
+                  path="/embed/:file"
+                  render={(props) => <MarkdownFilePage />}
+                />
+
+                <Route path="/amm_mining_zh">
+                  <Redirect to={{pathname: '/embed/amm_mining_9_zh.md'}} />
+                </Route>
+
+                <Route path="/amm_mining_en">
+                  <Redirect to={{pathname: '/embed/amm_mining_9_en.md'}} />
+                </Route>
+
+                <Route path="/amm_top25_en">
+                  <Redirect to={{pathname: '/embed/amm_top25_9_en.md'}} />
+                </Route>
+
+                <Route path="/amm_top25_zh">
+                  <Redirect to={{pathname: '/embed/amm_top25_9_zh.md'}} />
+                </Route>
+
+                <Route path="/orderbook_mining_zh">
+                  <Redirect
+                    to={{pathname: '/embed/orderbook_mining_19_zh.md'}}
                   />
-                )}
-              />
+                </Route>
 
-              <Route
-                path="/legal/contracts/en"
-                exact
-                render={(props) => (
-                  <SimpleMarkdownPage name="contracts" isChinese={false} />
-                )}
-              />
-              <Route
-                path="/legal/contracts/zh"
-                exact
-                render={(props) => (
-                  <SimpleMarkdownPage name="contracts" isChinese={true} />
-                )}
-              />
-              <Route
-                path="/legal/contracts"
-                exact
-                render={(props) => (
-                  <SimpleMarkdownPage name="contracts" isChinese={isChinese} />
-                )}
-              />
+                <Route path="/orderbook_mining_en">
+                  <Redirect
+                    to={{pathname: '/embed/orderbook_mining_19_en.md'}}
+                  />
+                </Route>
 
-              <Route
-                path="/legal/aboutl2/en"
-                exact
-                render={(props) => (
-                  <SimpleMarkdownPage name="aboutl2" isChinese={false} />
-                )}
-              />
-              <Route
-                path="/legal/aboutl2/zh"
-                exact
-                render={(props) => (
-                  <SimpleMarkdownPage name="aboutl2" isChinese={true} />
-                )}
-              />
-              <Route
-                path="/legal/aboutl2"
-                exact
-                render={(props) => (
-                  <SimpleMarkdownPage name="aboutl2" isChinese={isChinese} />
-                )}
-              />
-
-              <Route
-                path="/legal/points/en"
-                exact
-                render={(props) => (
-                  <SimpleMarkdownPage name="points" isChinese={false} />
-                )}
-              />
-              <Route
-                path="/legal/points/zh"
-                exact
-                render={(props) => (
-                  <SimpleMarkdownPage name="points" isChinese={true} />
-                )}
-              />
-              <Route
-                path="/legal/points"
-                exact
-                render={(props) => (
-                  <SimpleMarkdownPage name="points" isChinese={isChinese} />
-                )}
-              />
-
-              <Route
-                path="/embed/:file"
-                render={(props) => <MarkdownFilePage />}
-              />
-
-              <Route path="/amm_mining_zh">
-                <Redirect to={{pathname: '/embed/amm_mining_9_zh.md'}} />
-              </Route>
-
-              <Route path="/amm_mining_en">
-                <Redirect to={{pathname: '/embed/amm_mining_9_en.md'}} />
-              </Route>
-
-              <Route path="/amm_top25_en">
-                <Redirect to={{pathname: '/embed/amm_top25_9_en.md'}} />
-              </Route>
-
-              <Route path="/amm_top25_zh">
-                <Redirect to={{pathname: '/embed/amm_top25_9_zh.md'}} />
-              </Route>
-
-              <Route path="/orderbook_mining_zh">
-                <Redirect
-                  to={{pathname: '/embed/orderbook_mining_19_zh.md'}}
-                />
-              </Route>
-
-              <Route path="/orderbook_mining_en">
-                <Redirect
-                  to={{pathname: '/embed/orderbook_mining_19_en.md'}}
-                />
-              </Route>
-
-              <Route component={NotFound} />
-            </Switch>
-          </ScrollToTop>
-        </HashRouter>
-      </LocalizeProvider>
+                <Route component={NotFound} />
+              </Switch>
+            </ScrollToTop>
+          </HashRouter>
+        </LocalizeProvider>
+      </HttpsRedirect>
     );
   }
 }
