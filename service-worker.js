db7e1ef1534e32c221794d08902cba1f7e35b5e7
SunnyWheat <eth_dev@126.com>
2021-09-22 16:44:59 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "//loopring.org/precache-manifest.9f90539da02a14c29f2c862d70e712e3.js"
+  "//loopring.org/precache-manifest.1dc7661c3bd00ad0cf69c787883f308c.js"
 );
 
 self.addEventListener('message', (event) => {
