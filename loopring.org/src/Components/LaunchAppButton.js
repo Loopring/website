830428ff12c6480bff9ab965beff13ee9768e8e9
SunnyWheat <yuli.xiaodoudou@gmail.com>
2022-04-05 22:50:23 -0700

    Add LaunchAppSmallButtonStyled

--- loopring.org/src/Components/LaunchAppButton.js
+++ loopring.org/src/Components/LaunchAppButton.js
@@ -37,6 +37,42 @@ export const LaunchAppButtonButtonStyled = styled(Button)`
   }
 `;
 
+export const LaunchAppSmallButtonStyled = styled(Button)`
+  margin: auto 10px;
+  width: 133px;
+  height: 32px;
+  justify-content: space-around;
+  background: linear-gradient(94.92deg, #4169ff 0.91%, #a016c2 103.55%);
+  font-size: 16px !important;
+  color: #ffffff;
+  text-transform: none;
+  padding: 0;
+
+  div {
+    height: 32px;
+    line-height: 32px;
+  }
+
+  div:nth-child(2) {
+    display: flex;
+    align-items: center;
+  }
+
+  :hover {
+    color: #ffffff;
+  }
+
+  :hover:before {
+    background: rgba(255, 255, 255, 0.2);
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+  }
+`;
+
 export const LaunchAppButton = props => {
   return <div></div>;
 };
